83815c1bd62bc6dc1a0b4bfbb1ac659aac8134b8
Saghm Rossi <saghmrossi@gmail.com>
2016-03-25 17:38:08 -0400

    fix return type of native functions and add constructor

--- src/types/native_fn.rs
+++ src/types/native_fn.rs
@@ -4,7 +4,13 @@ use backend::Backend;
 use super::js_var::{JsPtrEnum, JsVar};
 
 #[derive(Clone)]
-pub struct NativeFn(fn(Box<Backend>, Vec<(JsVar, Option<JsPtrEnum>)>) -> JsVar);
+pub struct NativeFn(fn(Box<Backend>, Vec<(JsVar, Option<JsPtrEnum>)>) -> (JsVar, Option<JsPtrEnum>));
+
+impl NativeFn {
+  fn new(func: fn(Box<Backend>, Vec<(JsVar, Option<JsPtrEnum>)>) -> (JsVar, Option<JsPtrEnum>)) -> NativeFn {
+    NativeFn(func)
+  }
+}
 
 impl Debug for NativeFn {
     fn fmt(&self, fmt: &mut Formatter) -> fmt::Result {

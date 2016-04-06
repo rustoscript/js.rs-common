3b3ad163bca80060c560fc81119b6991318ff7f0
Saghm Rossi <saghmrossi@gmail.com>
2016-04-06 14:31:40 -0400

    add `get_allocator` method to Backend

--- src/backend.rs
+++ src/backend.rs
@@ -1,4 +1,8 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
 use gc_error::GcError;
+use types::allocator::Allocator;
 use types::binding::Binding;
 use types::js_var::{JsPtrEnum, JsVar};
 
@@ -6,4 +10,5 @@ pub trait Backend {
     fn alloc(&mut self, var: JsVar, ptr: Option<JsPtrEnum>) -> Result<Binding, GcError>;
     fn load(&mut self, bnd: &Binding) -> Result<(JsVar, Option<JsPtrEnum>), GcError>;
     fn store(&mut self, var: JsVar, ptr: Option<JsPtrEnum>) -> Result<(), GcError>;
+    fn get_allocator(&self) -> Rc<RefCell<Allocator<Error=GcError>>>;
 }
